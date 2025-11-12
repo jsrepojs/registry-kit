a4b0b0f57de13972d6d1a86a0f405bcc5b0d3696
Aidan Bleser <aidanbleser35@gmail.com>
2025-11-12 14:12:52 -0600

    better tabs styling method

--- src/lib/components/ui/demo/demo-tabs.svelte
+++ src/lib/components/ui/demo/demo-tabs.svelte
@@ -6,7 +6,22 @@
 	let { ref = $bindable(null), class: className, ...rest }: TabsPrimitive.ListProps = $props();
 </script>
 
-<Tabs.List data-slot="demo-tabs" bind:ref class={cn(className)} {...rest}>
-	<Tabs.Trigger value="preview">Preview</Tabs.Trigger>
-	<Tabs.Trigger value="code">Code</Tabs.Trigger>
+<Tabs.List
+	data-slot="demo-tabs"
+	bind:ref
+	class={cn('h-9 rounded-md border border-border bg-background', className)}
+	{...rest}
+>
+	<Tabs.Trigger
+		value="preview"
+		class="rounded-sm border-none bg-background data-[state=active]:bg-accent!"
+	>
+		Preview
+	</Tabs.Trigger>
+	<Tabs.Trigger
+		value="code"
+		class="rounded-sm border-none bg-background data-[state=active]:bg-accent!"
+	>
+		Code
+	</Tabs.Trigger>
 </Tabs.List>
